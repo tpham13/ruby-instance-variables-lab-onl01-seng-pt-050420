867d455a98bdfb0540705dcd881bea7c8bd90385
Thanh Pham <tpham0123@gmail.com>
2020-05-18 17:13:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,14 @@
 class Dog
   def name=(dog_name)
-    this_dogs_name = dog_name
+    this_dogs_name = dog_name     #name = is a setter
   end
  
   def name
-    this_dogs_name
+    this_dogs_name                # name is a getter 
   end
 end
 
+lassie = Dog.new 
+lass.name= ("Lassie")
+
+lassie.name 

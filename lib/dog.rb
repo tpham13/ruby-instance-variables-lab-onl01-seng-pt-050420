442afcb29f1845823c923da95f74dedd59e2f4d2
Thanh Pham <tpham0123@gmail.com>
2020-05-18 17:24:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
   def name=(dog_name)
-    this_dogs_name = dog_name     #name = is a setter
+    @this_dogs_name = dog_name     #name = is a setter;                 the @ allow us to access this                   variable outsideof this class 
   end
  
   def name
-    this_dogs_name            # name is a getter;                             this is a local variable 
+    @this_dogs_name            # name is a getter;                             this is a local variable 
   end
 end
 
